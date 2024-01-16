5126db88b7191066c6da212eeda493817847dd44
sakhawat71 <sakhawat.showrav@gmail.com>
2024-01-17 02:07:16 +0600

    project start

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -10,6 +10,7 @@ const Shop = () => {
     const [cart, setCart] = useState([])
 
     useEffect(() => {
+        
         fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => setProducts(data))
